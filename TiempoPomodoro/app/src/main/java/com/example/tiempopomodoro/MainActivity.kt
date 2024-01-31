165fc7e56b7ad7ca375d7a4cc2a9aad227eed4c2
ivan <icastellano2106@gmail.com>
2024-01-31 21:27:22 +0100

    31/01/2024

--- TiempoPomodoro/app/src/main/java/com/example/tiempopomodoro/MainActivity.kt
+++ TiempoPomodoro/app/src/main/java/com/example/tiempopomodoro/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.tiempopomodoro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiempopomodoro.databinding.ActivityMain2Binding
+import com.example.tiempopomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.sendButton.setOnClickListener{
+            val createBookIntent = Intent(
+                this, MainActivity2::class.java
+            )
+
+            startActivity(createBookIntent)
+        }
     }
 }
--- TiempoPomodoro/app/src/main/java/com/example/tiempopomodoro/MainActivity2.kt
+++ TiempoPomodoro/app/src/main/java/com/example/tiempopomodoro/MainActivity2.kt
@@ -1,11 +1,27 @@
 package com.example.tiempopomodoro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiempopomodoro.databinding.ActivityMain2Binding
+import com.example.tiempopomodoro.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.joinbutton.setOnClickListener{
+            val createBookIntent = Intent(
+                this, MainActivity::class.java
+            )
+
+            startActivity(createBookIntent)
+        }
+
+
     }
+
 }
